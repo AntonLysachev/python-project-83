1c0f7c475dc40d0a1074c18e276154d8c29d60ea
AntonLysachev <anton@lysachev.ru>
2024-02-21 15:28:14 +0500

    add index

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -16,6 +16,10 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 def index():
     return render_template('index.html')
 
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
