419dafec28c078699ec78be125b82f0245d6bb7b
AntonLysachev <anton@lysachev.ru>
2024-02-26 11:28:14 +0500

    try

--- page_analyzer/CRUD/db_util.py
+++ page_analyzer/CRUD/db_util.py
@@ -3,9 +3,9 @@ import os
 
 
 def get_connection() -> psycopg2.connect:
-    try:
-        database_url = os.getenv('DATABASE_URL')
-        connection = psycopg2.connect(database_url)
-    except (Exception) as error:
-        print(error)
+    # try:
+    database_url = os.getenv('DATABASE_URL')
+    connection = psycopg2.connect(database_url)
+    # except (Exception) as error:
+    #     print(error)
     return connection
