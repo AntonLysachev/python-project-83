4021986b5549e5cf221103cf8b47052f5984c6a1
AntonLysachev <anton@lysachev.ru>
2024-03-04 11:55:30 +0500

    delete try in db_util

--- page_analyzer/CRUD/db_util.py
+++ page_analyzer/CRUD/db_util.py
@@ -3,9 +3,6 @@ import os
 
 
 def get_connection() -> psycopg2.connect:
-    try:
-        database_url = os.getenv('DATABASE_URL')
-        connection = psycopg2.connect(database_url)
-    except (Exception) as error:
-        print(error)
+    database_url = os.getenv('DATABASE_URL')
+    connection = psycopg2.connect(database_url)
     return connection
