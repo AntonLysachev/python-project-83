f3d797c2b4863326fcdff58d9bc629467ab04d95
AntonLysachev <anton@lysachev.ru>
2024-03-12 17:39:00 +0500

    rename var html -> response

--- page_analyzer/html_content.py
+++ page_analyzer/html_content.py
@@ -1,9 +1,9 @@
 from bs4 import BeautifulSoup
 
 
-def get_info_site(html) -> tuple:
-    status_code = html.status_code
-    soup = BeautifulSoup(html.content, 'html.parser')
+def get_info_site(response) -> tuple:
+    status_code = response.status_code
+    soup = BeautifulSoup(response.content, 'html.parser')
     h1 = soup.h1.string if soup.h1 else ''
     title = soup.title.string if soup.title else ''
     description_tag = soup.find('meta', attrs={'name': 'description'})
